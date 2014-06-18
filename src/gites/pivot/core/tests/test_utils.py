7fff795aa009248eca94ebd897b5d7e17c0c3664
Martin Peeters <martin.peeters@affinitic.be>
2014-06-18 14:05:30 +0200

    Add functions to compare two objects attributes

---
+++ src/gites/pivot/core/tests/test_utils.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+"""
+gites.pivot.core
+
+Created by mpeeters
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+
+from gites.pivot.core import testing
+from gites.pivot.core import utils
+
+
+class TestUtils(testing.PivotDBTestCase):
+
+    def test_get_differences(self):
+        obj1 = type('obj', (object, ), {'a': 1, 'b': 2, 'c': 3})()
+        obj2 = type('obj', (object, ), {'a': 3, 'b': 2, 'c': 1})()
+        expected = [('a', 1, 3), ('c', 3, 1)]
+        result = utils.get_differences(obj1, obj2, ('a', 'b', 'c'))
+        self.assertEqual(expected, result)
+
+    def test_get_differences_with_missing_attributes(self):
+        obj1 = type('obj', (object, ), {'a': 1, 'b': 2})()
+        obj2 = type('obj', (object, ), {'a': 1})()
+        expected = [('b', 2, None)]
+        result = utils.get_differences(obj1, obj2, ('a', 'b'))
+        self.assertEqual(expected, result)
+
+    def test_get_differences_with_empty_strings(self):
+        obj1 = type('obj', (object, ), {'a': ' ', 'b': None})()
+        obj2 = type('obj', (object, ), {'a': None, 'b': ''})()
+        expected = []
+        result = utils.get_differences(obj1, obj2, ('a', 'b'))
+        self.assertEqual(expected, result)
+
+    def test_is_equal(self):
+        self.assertTrue(utils.is_equal('FOO', 'foo'))
+        self.assertFalse(utils.is_equal('foo', None))
+
+    def test_clean_value(self):
+        self.assertEqual(None, utils.clean_value(None))
+        self.assertEqual('foo', utils.clean_value(' foo '))
---
+++ src/gites/pivot/core/utils.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+"""
+gites.pivot.core
+
+Created by mpeeters
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+
+
+def get_differences(obj1, obj2, attributes):
+    """Get the differences between two object limited to a list of given
+    attributes"""
+    diff = []
+    for attr in attributes:
+        obj1_value = clean_value(getattr(obj1, attr, None)) or None
+        obj2_value = clean_value(getattr(obj2, attr, None)) or None
+        if is_equal(obj1_value, obj2_value) is False:
+            diff.append((attr, obj1_value, obj2_value))
+    return diff
+
+
+def is_equal(value1, value2):
+    """Compare two value and return a boolean"""
+    if hasattr(value1, 'lower') and hasattr(value2, 'lower'):
+        return value1.lower() == value2.lower()
+    return value1 == value2
+
+
+def clean_value(value):
+    """Format value for comparisson"""
+    if hasattr(value, 'strip'):
+        return value.strip()
+    return value
