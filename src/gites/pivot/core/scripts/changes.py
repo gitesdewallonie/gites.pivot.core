8e6c166f5ab5460f36cf7a850d5018d37e252d3f
FBruynbroeck <francois@affinitic.be>
2014-06-19 17:31:38 +0200

    Update tests for PivotChanges script (no data for this moment...) refs #5832

--- src/gites/pivot/core/scripts/changes.py
+++ src/gites/pivot/core/scripts/changes.py
@@ -8,8 +8,10 @@ Copyright by Affinitic sprl
 """
 
 from affinitic.db.interfaces import IDatabase
-from affinitic.db.utils import initialize_declarative_mappers, initialize_defered_mappers
+from affinitic.db.utils import (initialize_declarative_mappers,
+                                initialize_defered_mappers)
 from gites.core.scripts.db import parseZCML
+from gites.pivot.core.utils import get_differences
 from gites.db import DeclarativeBase
 from gites.db.content import Hebergement
 from gites.pivot.db.content import HebergementView
@@ -17,6 +19,37 @@ from zope.component import getUtility
 
 import argparse
 import gites.pivot.core
+
+HEBCOLUMNS = ['heb_nom',
+              'heb_adresse',
+              'com_cp',
+              'heb_localite',
+              'com_nom',
+              'prov_nom',
+              'heb_maison_tourisme_fk',
+              'heb_gps_long',
+              'heb_gps_lat',
+              'heb_nombre_epis',
+              'heb_cgt_cap_min',
+              'heb_cgt_cap_max',
+              'heb_cgt_nbre_chmbre',
+              'heb_descriptif',
+              'heb_descriptif_nl',
+              'heb_descriptif_uk',
+              'heb_descriptif_de',
+              'heb_pointfort',
+              'heb_pointfort_nl',
+              'heb_pointfort_uk',
+              'heb_pointfort_de',
+              'heb_gid_access_tous',
+              'heb_animal',
+              'heb_fumeur',
+              'heb_date_creation',
+              'heb_date_modification',
+              'heb_lit_sup',
+              'heb_lit_1p',
+              'heb_lit_2p',
+              'heb_lit_enf']
 
 
 def main():
@@ -36,6 +69,23 @@ class PivotChanges(object):
         self.pg_session = getUtility(IDatabase, 'postgres').session
 
     def process(self):
+        differences = self.compareGitesWithPivot() or []
+        for diff in differences:
+            self.insertNotification(diff[1],
+                                    diff[2],
+                                    diff[0])
+
+    def compareGitesWithPivot(self):
+        # XXX Changer la date
+        from datetime import datetime
+        self.getHebergementsCGT()
+        hebsPivot = self.getLastChanges(datetime.now())
+        for hebPivot in hebsPivot:
+            heb = Hebergement.first(heb_code_cgt=hebPivot.heb_code_cgt)
+            if heb:
+                return get_differences(heb, hebPivot, HEBCOLUMNS)
+
+    def insertNotification(self, obj1, obj2, attr):
         pass
 
     def getHebergementsCGT(self):
--- src/gites/pivot/core/scripts/tests/test_changes.py
+++ src/gites/pivot/core/scripts/tests/test_changes.py
@@ -13,14 +13,20 @@ from gites.pivot.core.scripts.changes import PivotChanges
 
 class TestChanges(testing.PivotDBTestCase):
     pivot_sql_file = ('toffres')
+    gites_sql_file = ('hebergement')
 
     def test_getHebergementsCGT(self):
         changes = PivotChanges(None)
         hebergements = changes.getHebergementsCGT()
-        self.assertEqual(hebergements, ['1', '2'])
+        self.assertEqual(hebergements, [])
 
     def test_getLastChanges(self):
         date = datetime(2014, 6, 1, 0, 0)
         changes = PivotChanges(None)
         lastChanges = changes.getLastChanges(date)
         self.assertEqual(len(lastChanges), 1)
+
+    def test_compareGitesWithPivot(self):
+        changes = PivotChanges(None)
+        differences = changes.compareGitesWithPivot()
+        self.assertEqual(differences, None)
